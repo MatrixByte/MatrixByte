19844b12fe5a4d44c151d027a7b01d22d30f1530
MatrixByte <matrix-byte@outlook.com>
2023-08-14 10:51:59 +0800

    Created files

---
+++ app/deps.ts
@@ -0,0 +1,4 @@
+// File: deps.ts
+// Author: Matrix byte.
+
+export { Application, Router } from "https://deno.land/x/oak@v12.6.0/mod.ts";
---
+++ app/main.ts
@@ -0,0 +1,49 @@
+// File: main.ts
+// Author: Matrix byte.
+
+import { Application, Router } from "./deps.ts";
+
+const router = new Router();
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+router.get("/", async (ctx, next) => {
+    try {
+        await ctx.send({
+            root: `${Deno.cwd()}/app/static/`,
+            index: "index.html",
+        });
+    } catch (error) {
+        console.error(error.message);
+        await next();
+    }
+});
+
+router.get("/static/:file*", async (ctx, next) => {
+    const filename = ctx.params["file"];
+    try {
+        await ctx.send({
+            root: `${Deno.cwd()}/app/static/`,
+            path: filename,
+        });
+    } catch (error) {
+        console.error(error.message);
+        await next();
+    }
+});
+
+router.get("/readme", async (ctx, next) => {
+    try {
+        await ctx.send({
+            root: `${Deno.cwd()}/`,
+            path: "README.md",
+        });
+    } catch (error) {
+        console.error(error.message);
+        await next();
+    }
+});
+
+await app.listen({ port: 3000 });
